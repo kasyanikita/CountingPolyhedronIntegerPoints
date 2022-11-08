f02d84a7ed8c342e2628cbfcfad97059d1bdcb23
Nikita Kasyanov <kasyanikita@gmail.com>
2022-11-09 02:28:34 +0300

    Change structure of ExpPoly class

--- include/ExpPoly.h
+++ include/ExpPoly.h
@@ -6,42 +6,73 @@ namespace GroupIP
 {
     class ExpPoly
     {
-        std::vector<double> _base;
-        std::vector<double> _exponent;
+        std::unordered_map<double, double> poly;
 
     public:
-        friend ExpPoly operator*(double c, ExpPoly &x);
-        friend ExpPoly operator*(ExpPoly &x, double c);
-        ExpPoly(const std::vector<double> &exp) : _exponent(exp), _base(exp.size(), 1)
+        friend ExpPoly operator*(double c, ExpPoly &exp_poly);
+        friend ExpPoly operator*(ExpPoly &exp_poly, double c);
+        ExpPoly(const std::vector<double> &exp, const std::vector<double> &base)
         {
+            assert(exp.size() == base.size());
+            for (size_t i = 0; i < exp.size(); ++i)
+            {
+                auto it = poly.find(exp[i]);
+                if (it != poly.end())
+                {
+                    poly[exp[i]] += base[i];
+                }
+                else
+                {
+                    poly.insert(std::make_pair(exp[i], base[i]));
+                }
+            }
         }
         ExpPoly operator+(ExpPoly &rhs) const
         {
-            assert(_exponent.size() == rhs._exponent.size());
+            ExpPoly res(*this);
+            for (auto [k, v] : rhs.poly) {
+                auto it = res.poly.find(k);
+                if (it != res.poly.end()) {
+                    res.poly[k] += v;
+                } else {
+                    res.poly[k] = v;
+                }
+            }
+            return res;
         }
 
-        const std::vector<double>& get_base() const {
-            return _base;
-        }
-
-        const std::vector<double>& get_exponent() const {
-            return _exponent;
+        void print_poly()
+        {
+            for (auto [k, v] : poly)
+            {
+                if (v == 1)
+                {
+                    std::cout << "e^" << k << "x"
+                              << " ";
+                }
+                else
+                {
+                    std::cout << v << "*e^" << k << "x"
+                              << " ";
+                }
+            }
+            std::cout << '\n';
         }
     };
 
-    ExpPoly operator*(double c, ExpPoly &poly)
+    ExpPoly operator*(double c, ExpPoly &exp_poly)
     {
-        ExpPoly res(poly._exponent);
-        for (auto &x : res._base)
+        ExpPoly res(exp_poly);
+        for (auto [k, v] : res.poly)
         {
-            x *= c;
+            res.poly[k] *= c;
         }
         return res;
     }
 
-    ExpPoly operator*(ExpPoly &poly, double c)
+    ExpPoly operator*(ExpPoly &exp_poly, double c)
     {
-        return c * poly;
+        return c * exp_poly;
     }
 
 } // namespace GroupIP
--- include/global_defs.h
+++ include/global_defs.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 #include <string>
 #include <vector>
 #include <algorithm>
